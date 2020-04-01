a3f555f263a69da98f45f063fa8d939f28bae0fd
R Sánchez <rene.as.himself@gmail.com>
2020-03-31 22:36:32 -0600

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  let name = "Yoshi"
+  let beltColor = "black"
+
+  const handleClick = () => {
+    beltColor = "orange"
+  }
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>{beltColor}</p>
+  <button on:click={handleClick}>Update belt color</button>
+</main>
+
+<style type="text/scss">
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
